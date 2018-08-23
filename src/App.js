c0be7912f7cd7926b8ea3739937cdfe6b5be82b6
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-23 13:26:32 +0200

    calendar ui updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Calendar />
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>react 
+            <b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
       </div>
     );
   }
